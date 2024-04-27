d316965eedac259bd5578488bad4190fcf3dddb7
FelypeInvictus <felype.sales@outlook.com>
2024-04-27 12:58:04 -0300

    Upgrade App

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:imc/main.dart';
@@ -14,7 +14,7 @@ import 'package:imc/utils/imc_calculo.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ImcApp());
 
     // Verify that our counter starts at 0.
 
